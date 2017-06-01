a6eab1721b1ef56d950ef85ce2c378ffda8b1cd9
Alice Bevan-McGregor <alice@gothcandy.com>
2017-06-01 14:23:41 -0400

    Package metadata.

---
+++ setup.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import os
+import sys
+import codecs
+
+try:
+	from setuptools.core import setup, find_packages
+except ImportError:
+	from setuptools import setup, find_packages
+
+
+if sys.version_info < (2, 7):
+	raise SystemExit("Python 2.7 or later is required.")
+elif sys.version_info > (3, 0) and sys.version_info < (3, 2):
+	raise SystemExit("Python 3.2 or later is required.")
+
+version = description = url = author = version_info = ''  # Actually loaded on the next line; be quiet, linter.
+exec(open(os.path.join("uri", "release.py")).read())
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+tests_require = [
+		'pytest',  # test collector and extensible runner
+		'pytest-cov',  # coverage reporting
+		'pytest-flakes',  # syntax validation
+		'pytest-catchlog',  # log capture
+		'pytest-isort',  # import ordering
+	]
+
+trove_map = {
+		'plan': "Development Status :: 1 - Planning",
+		'alpha': "Development Status :: 3 - Alpha",
+		'beta': "Development Status :: 4 - Beta",
+		'final': "Development Status :: 5 - Production/Stable",
+	}
+
+
+# # Entry Point
+
+setup(
+	name = "uri",
+	version = version,
+	description = description,
+	long_description = codecs.open(os.path.join(here, 'README.rst'), 'r', 'utf8').read(),
+	url = url,
+	author = author.name,
+	author_email = author.email,
+	license = 'MIT',
+	keywords = ['type', 'URI', 'URL', 'rfc', 'rfc'],
+	classifiers = trove_map[version_info.releaselevel] + [
+			"Intended Audience :: Developers",
+			"License :: OSI Approved :: MIT License",
+			"Operating System :: OS Independent",
+			"Programming Language :: Python",
+			"Programming Language :: Python :: 2",
+			"Programming Language :: Python :: 2.7",
+			"Programming Language :: Python :: 3",
+			"Programming Language :: Python :: 3.2",
+			"Programming Language :: Python :: 3.3",
+			"Programming Language :: Python :: 3.4",
+			"Programming Language :: Python :: 3.5",
+			"Programming Language :: Python :: 3.6",
+			"Programming Language :: Python :: Implementation :: CPython",
+			"Programming Language :: Python :: Implementation :: PyPy",
+			"Topic :: Software Development :: Libraries :: Python Modules",
+			"Topic :: Utilities"
+		],
+	
+	packages = find_packages(exclude=['test', 'example', 'benchmark', 'htmlcov']),
+	include_package_data = True,
+	package_data = {'': ['README.rst', 'LICENSE.txt']},
+	namespace_packages = ['marrow', 'web', 'web.session'],
+	zip_safe = False,
+	
+	# ## Dependency Declaration
+	
+	setup_requires = [
+			'pytest-runner',
+		] if {'pytest', 'test', 'ptr'}.intersection(sys.argv) else [],
+	
+	install_requires = [
+			'marrow.schema>=1.2.0,<2.0.0',  # Declarative schema support.
+			'marrow.package>=1.1.0,<2.0.0',  # Plugin discovery and loading.
+			'pymongo>=3.2',  # We require modern API.
+			'pathlib2; python_version < "3.4"',  # Path manipulation utility.
+		],
+	
+	extras_require = dict(
+			http = ['requests'],  # Support for the http:// and https:// protocols.
+			development = tests_require + [  # Development-time dependencies.
+					'pre-commit',  # Commit hooks for code quality.
+				],
+		),
+	
+	tests_require = tests_require,
+	
+	# ## Plugin Registration
+	
+	entry_points = {
+				'uri.scheme': [  # Session Engine
+						'http = uri.scheme.http:HTTPScheme[http]',
+						'https = uri.scheme.http:HTTPSScheme[http]',
+						'ftp = uri.scheme.ftp:FTPScheme',
+						'file = uri.scheme.file:FileScheme',
+					],
+			},
+)
