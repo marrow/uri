1349f01e325d0033511f4b74b3679a21ddaf2f7b
Alice Bevan-McGregor <alice@gothcandy.com>
2017-06-02 03:51:26 -0400

    Add version information to primary module.

--- uri/__init__.py
+++ uri/__init__.py
@@ -2,6 +2,8 @@
 
 """A type to represent, query, and manipulate a Uniform Resource Identifier."""
 
+from .release import version as __version__
+
 
 __all__ = [
 	]  # TODO: Re-watch that Python import internals presentation for the semi-auto way to do this.
