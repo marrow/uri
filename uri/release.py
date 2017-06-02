c65179c8db871b3395b6d5aaae0cd8b109524dd4
Alice Bevan-McGregor <alice@gothcandy.com>
2017-06-02 03:28:33 -0400

    Add missing release file.

---
+++ uri/release.py
@@ -0,0 +1,19 @@
+# encoding: utf-8
+
+"""Release information for the uri package."""
+
+from __future__ import unicode_literals
+
+from collections import namedtuple
+
+level_map = {'plan': '.dev'}
+
+version_info = namedtuple('version_info', ('major', 'minor', 'micro', 'releaselevel', 'serial'))(2, 0, 0, 'plan', 1)
+version = ".".join([str(i) for i in version_info[:3]]) + \
+		((level_map.get(version_info.releaselevel, version_info.releaselevel[0]) + \
+		str(version_info.serial)) if version_info.releaselevel != 'final' else '')
+
+author = namedtuple('Author', ['name', 'email'])("Alice Bevan-McGregor", 'alice@gothcandy.com')
+
+description = "A type to represent, query, and manipulate a Uniform Resource Identifier."
+url = 'https://github.com/marrow/uri/'
