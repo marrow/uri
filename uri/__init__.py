e577601e00006844f6f45e694c443ff63882f22e
Alice Bevan-McGregor <alice@gothcandy.com>
2017-06-02 03:30:13 -0400

    Removed reference to missing module.

--- uri/__init__.py
+++ uri/__init__.py
@@ -2,9 +2,6 @@
 
 """A type to represent, query, and manipulate a Uniform Resource Identifier."""
 
-from .qso import QSO
-
 
 __all__ = [
-		'QSO'
 	]  # TODO: Re-watch that Python import internals presentation for the semi-auto way to do this.
