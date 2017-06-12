a640c1ec381ed7c6a8614cd7380809c4579798b2
Alice Bevan-McGregor <alice@gothcandy.com>
2017-06-12 01:04:04 -0400

    Version bump.

--- uri/release.py
+++ uri/release.py
@@ -8,7 +8,7 @@ from collections import namedtuple
 
 level_map = {'plan': '.dev'}
 
-version_info = namedtuple('version_info', ('major', 'minor', 'micro', 'releaselevel', 'serial'))(2, 0, 0, 'plan', 1)
+version_info = namedtuple('version_info', ('major', 'minor', 'micro', 'releaselevel', 'serial'))(2, 0, 0, 'final', 0)
 version = ".".join([str(i) for i in version_info[:3]]) + \
 		((level_map.get(version_info.releaselevel, version_info.releaselevel[0]) + \
 		str(version_info.serial)) if version_info.releaselevel != 'final' else '')
