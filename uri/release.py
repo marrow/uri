75483a5c3bceb5cea90b629668087d99110ae2a5
Alice Bevan-McGregor <alice@gothcandy.com>
2021-03-24 10:59:21 -0400

    Version bump.

--- uri/release.py
+++ uri/release.py
@@ -1,17 +1,17 @@
 # encoding: utf-8
+# type: ignore
 
 """Release information for the uri package."""
-
-from __future__ import unicode_literals
 
 from collections import namedtuple
 
 level_map = {'plan': '.dev'}
 
-version_info = namedtuple('version_info', ('major', 'minor', 'micro', 'releaselevel', 'serial'))(3, 0, 0, 'final', 0)
+version_info = namedtuple('version_info', ('major', 'minor', 'micro', 'releaselevel', 'serial')) \
+		(3, 0, 0, 'final', 0)
 version = ".".join([str(i) for i in version_info[:3]]) + \
 		((level_map.get(version_info.releaselevel, version_info.releaselevel[0]) + \
-		str(version_info.serial)) if version_info.releaselevel != 'final' else '')
+		str(version_info.serial)) if version_info.releaselevel != 'final' else '')  
 
 author = namedtuple('Author', ['name', 'email'])("Alice Bevan-McGregor", 'alice@gothcandy.com')
 
