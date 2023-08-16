4fe87644358e38455c08524596b419227a21cb23
Alice Zoë Bevan–McGregor <alice@gothcandy.com>
2023-08-16 11:59:58 -0400

    Dynamic package metadata discovery.

--- uri/__init__.py
+++ uri/__init__.py
@@ -1,16 +1,23 @@
 """A type to represent, query, and manipulate a Uniform Resource Identifier."""
 
+from importlib.metadata import metadata as _metadata, PackageNotFoundError as _NotFound
 from pathlib import PurePosixPath as Path
+
+__all__ = set(locals())
 
 from .bucket import Bucket
 from .qso import QSO
 from .uri import URI
 
-__version__ = ''
+try:
+	_package = _metadata('uri')
+	__version__ = _package.get('version')
+	__author__ = _package.get('author-email')
+	del _package
+except _NotFound:
+	__version__ = 'dev'
+	__author__ = "Local Development"
 
-__all__ = [
-		'Path',
-		'Bucket',
-		'QSO',
-		'URI',
-	]  # TODO: Re-watch that Python import internals presentation for the semi-auto way to do this.
+__all__ = set(i for i in locals() if not i.startswith('_')) - __all__
+__license__ = 'MIT'
+
