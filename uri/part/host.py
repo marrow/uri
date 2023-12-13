a7d257143827a48a1bd729de95900ec1ba1a4b29
Alice Zoë Bevan–McGregor <alice@gothcandy.com>
2023-12-12 23:34:30 -0500

    Implement normalization explicitly.

--- uri/part/host.py
+++ uri/part/host.py
@@ -1,4 +1,6 @@
+from functools import partial
 from socket import inet_pton, AF_INET6, error as SocketError
+from typing import Any, Optional, Union
 
 from .base import ProxyPart
 
@@ -8,10 +10,23 @@ class HostPart(ProxyPart):
 	
 	attribute = '_host'
 	
+	def cast(self, value:str) -> str:
+		value = value.rstrip('.')  # Remove extraneous "DNS root authority" notation.
+		
+		if value.startswith('xn--'):  # Process IDNA - internationalized domain names.
+			value = value.encode('ascii').decode('idna')
+		
+		return value
+	
 	def render(self, obj, value):
 		result = super(HostPart, self).render(obj, value)
 		
 		if result:
+			try:
+				result.encode('ascii')
+			except UnicodeEncodeError:
+				result = result.encode('idna').decode('ascii')
+			
 			try:  # Identify and armour IPv6 address literals.
 				inet_pton(AF_INET6, value)
 			except SocketError:
