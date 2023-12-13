e9aa947d9d72a3d77e06cda23f210ce8c0b7890a
Alice Zoë Bevan–McGregor <alice@gothcandy.com>
2023-12-12 23:34:16 -0500

    Annotations, documentation, tweaked plugin loading.

--- uri/part/scheme.py
+++ uri/part/scheme.py
@@ -1,5 +1,5 @@
 from importlib.metadata import entry_points
-from typing import ClassVar, Dict, Optional
+from typing import Any, ClassVar, Dict, Optional, Union  #, Self
 from re import compile as r, Pattern
 
 from .base import Part
@@ -9,11 +9,16 @@ from ..scheme import Scheme
 class SchemePart(Part):
 	__slots__: tuple = ()  # Do not populate a __dict__ dictionary attribute; only allocate space for these.
 	
-	registry: ClassVar[Dict[str, Optional[Scheme]]] = {'': None}
+	registry: ClassVar[Dict[str, Optional[Scheme]]] = {'': None}  # Singleton cache of Scheme instances, by name.
 	suffix: str = ':'  # Protocol suffix when utilized as part of a complete URI; e.g. ':' or '://'.
-	valid: Pattern = r(r'[a-z][a-z0-9+.+-]*')  # Protocol/scheme name validated when run unoptimized.
+	valid: Pattern = r(r'[a-z][a-z0-9+.+-]*')  # Protocol/scheme name validated when run without optimization.
 	
 	def load(self, plugin:str) -> Scheme:
+		"""Attempt to retrieve a Scheme for the given named protocol.
+		
+		Utilizes a cache, which results in URI utilizing singletons of each named protocol.
+		"""
+		
 		assert self.valid.match(plugin), f"Invalid plugin name: {plugin!r}"
 		if plugin in self.registry: return self.registry[plugin]  # Short circuit if we've seen this before.
 		
@@ -22,11 +27,13 @@ class SchemePart(Part):
 		except KeyError: result = Scheme(plugin)  # Can't look up by registered name? It's generic.
 		else: result = result(plugin)  # Otherwise, instantiate the subclass, informing it of its name.
 		
-		self.registry[plugin] = result  # Record the instance in a local registry / cache.
+		self.registry[plugin] = result  # Record the instance in a local registry / cache and return it.
 		
 		return result
 	
-	def render(self, obj, value) -> str:
+	def render(self, obj:Any, value:str) -> str:
+		"""Render the scheme component of a whole URI."""
+		
 		result = super(SchemePart, self).render(obj, value)
 		
 		if obj._scheme and obj.scheme.slashed:
@@ -37,16 +44,15 @@ class SchemePart(Part):
 		
 		return result
 	
-	def __get__(self, obj, cls=None):
+	def __get__(self, obj:Any, cls:Optional[Any]=None) -> Optional[Union['SchemePart', Scheme]]:
+		"""Accessed as a class attribute, return this instance, otherwise decant a Scheme from the containing object."""
+		
 		if obj is None: return self
-		scheme = obj._scheme
-		
-		if scheme is not None:
-			scheme = self.load(scheme)
-		
-		return scheme
+		return None if obj._scheme is None else self.load(obj._scheme)
 	
-	def __set__(self, obj, value):
+	def __set__(self, obj:Any, value:Optional[Union[str,bytes]]) -> None:
+		"""Assign a new named scheme to this URI."""
+		
 		if isinstance(value, bytes):
 			value = value.decode('ascii')
 		
@@ -54,4 +60,4 @@ class SchemePart(Part):
 			obj._scheme = None
 			return
 		
-		obj._scheme = Scheme(value).name
+		obj._scheme = self.load(value).name  # This gives the plugin registry a chance to normalize the recorded name.
