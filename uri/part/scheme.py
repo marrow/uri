53f87ef2a5c8fc972c602c54d51dc431a76795fe
Nikos Koukis <nikolaos@slamcore.com>
2024-01-24 22:23:47 +0200

    Add to .gitignore

--- uri/part/scheme.py
+++ uri/part/scheme.py
@@ -25,7 +25,6 @@ class SchemePart(Part):
 
         # If we haven't, attempt to load the explicit Scheme subclass to utilize for this named scheme.
         try:
-            import pdb; pdb.set_trace()
             result = entry_points(group="uri.scheme")[plugin].load()
         except KeyError:
             result = Scheme(plugin)  # Can't look up by registered name? It's generic.
